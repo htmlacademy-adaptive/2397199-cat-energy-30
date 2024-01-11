5aa9d025f1ca3c7cd90f3fe3bb62d00034af82fd
aandreei <andreeisyhov@gmail.com>
2024-01-11 22:36:08 +0500

    Добавляет сетку для мобильной версии index-а

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,17 @@
-/* в этот файл добавляет скрипты*/
+// Бургер в навигации
+let navMain = document.querySelector('.main-nav');
+let navToggle = document.querySelector('.main-nav__toggle');
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
+
+// Слайдер на главной странице
